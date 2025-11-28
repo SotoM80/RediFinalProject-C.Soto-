c91252305d84ab6b6984d1371d577f73bcd93eab
Christian Soto Muñoz <christiansotomunoz@MAC.local>
2025-11-28 18:03:58 +0100

    today i finish fetch

---
+++ js/btn-responsive.js
@@ -0,0 +1,13 @@
+
+/*her is the menu */
+function myFunctionbtn() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+/*here is the btn for recipes */
