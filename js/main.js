58d07e97981b4f53fc81261e4ffe591dccc859c8
Christian Soto Muñoz <christiansotomunoz@MAC.local>
2025-11-28 18:07:04 +0100

    this is perfil from chef

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-//first 
+const perfiles = [
+  {
+   nombre: "Ana",
+    edad: 30,
+    ciudad: "Madrid",
+    descripcion: "nacido en lalal con  hfhhfhshfhhsfh kfsjfjjf uhdbfsuhbfushbush",
+    imageURl: './assets/images/yo.jpeg'
+    
+  
+  },
+  {
+    
+    nombre: "Luis",
+    edad: 25,
+    ciudad: "Barcelona",
+    descripcion: "nacido en lalal con  hfhhfhshfhhsfh kfsjfjjf uhdbfsuhbfushbush",
+    imageURl: './assets/images/yo.jpeg'
+
+  },
+  {
+     
+    nombre: "Sofía",
+    edad: 28,
+    ciudad: "Valencia",
+    descripcion: "nacido en lalal con  hfhhfhshfhhsfh kfsjfjjf uhdbfsuhbfushbush",
+    imageURl: './assets/images/yo.jpeg'
+  },
+  {
+     
+    nombre: "manuel",
+    edad: 28,
+    ciudad: "Valencia",
+    descripcion: "nacido en lalal con  hfhhfhshfhhsfh kfsjfjjf uhdbfsuhbfushbush",
+    imageURl: './assets/images/yo.jpeg'
+  }
+];
+
+perfiles.forEach(perfil => {
+console.log(`Nombre: ${perfil.nombre}, Edad: ${perfil.edad}, Ciudad: ${perfil.ciudad}, imagen: ${perfil.imagen}`);
+
+});
